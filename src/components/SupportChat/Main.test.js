3038f988910489cd6227712f654fa359660b5aba
Jamessom Queiroz <jamessom.queiroz@klickpages.com.br>
2020-08-06 15:19:34 -0300

    Adiciona componente de support chat

---
+++ src/components/SupportChat/Main.test.js
@@ -0,0 +1,112 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import SupportChat from './Main.vue';
+
+const $notify = {
+  error: jest.fn(),
+  success: jest.fn(),
+};
+
+const $t = jest.fn().mockImplementation((str) => str);
+
+describe('@/components/SupportChat/Main', () => {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  let wrapper;
+
+  const modules = {
+    SupportChat: {
+      actions: {
+        getChat: jest.fn(),
+      },
+      namespaced: true,
+    },
+  };
+
+  describe('when component is mounted', () => {
+    describe('when there is no support chat in the page', () => {
+      beforeAll(() => {
+        const store = new Vuex.Store({
+          modules,
+        });
+
+        wrapper = shallowMount(SupportChat, {
+          localVue,
+          store,
+          mocks: { $t, $notify },
+        });
+      });
+
+      afterAll(() => {
+        wrapper.destroy();
+        jest.clearAllMocks();
+      });
+
+      it('should render html correctly', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      });
+    });
+
+    describe('when there is a support chat in the page', () => {
+      beforeAll(() => {
+        const mockReturnData = {
+          data: '<script>sample data</script>',
+        };
+
+        modules.SupportChat.actions = {
+          getChat: jest.fn().mockImplementation(() => Promise.resolve(mockReturnData)),
+        };
+
+        const store = new Vuex.Store({
+          modules,
+        });
+
+        wrapper = shallowMount(SupportChat, {
+          localVue,
+          store,
+          mocks: { $t, $notify },
+        });
+      });
+
+      afterAll(() => {
+        wrapper.destroy();
+        jest.clearAllMocks();
+      });
+
+      it('should render html correctly', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      });
+    });
+
+    describe('when there is no support chat in the page', () => {
+      beforeAll(() => {
+        modules.SupportChat.actions = {
+          getChat: jest.fn().mockImplementation(() => Promise.reject()),
+        };
+
+        const store = new Vuex.Store({
+          modules,
+        });
+
+        wrapper = shallowMount(SupportChat, {
+          localVue,
+          store,
+          mocks: { $t, $notify },
+        });
+      });
+
+      afterAll(() => {
+        wrapper.destroy();
+        jest.clearAllMocks();
+      });
+
+      it('should call Notify.error', () => {
+        expect($notify.error).toHaveBeenCalledWith({
+          message: 'Error message',
+          autoHide: true,
+        });
+      });
+    });
+  });
+});
---
+++ src/components/SupportChat/Main.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="support-chat"></div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import { setklickartURL } from '@/config/klickart';
+
+export default {
+  name: 'SupportChat',
+  props: {
+    klickartURL: String,
+  },
+  methods: mapActions({
+    getChat: 'SupportChat/getChat',
+  }),
+  computed: {
+    ...mapGetters({
+      chat: 'SupportChat/chat',
+    }),
+    hasSupportChat() {
+      const chatWrapperClass = 'md-chat-widget-wrapper';
+      return !!document.body.querySelector(`.${chatWrapperClass}`);
+    },
+  },
+  async mounted() {
+    setklickartURL(this.klickartURL);
+
+    if (this.hasSupportChat) {
+      return;
+    }
+
+    try {
+      const chatScript = await this.getChat();
+
+      const HTMLChatFragment = document
+        .createRange()
+        .createContextualFragment(chatScript);
+
+      this.$el.appendChild(HTMLChatFragment);
+    } catch (error) {
+      this.$notify.error({
+        message: 'Error message',
+        autoHide: true,
+      });
+    }
+  },
+};
+</script>
